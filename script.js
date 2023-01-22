28a93593dfc0be5b49c0072ebc8920e0dbbdb145
William Bebiano <william.bebiano@hotmail.com>
2023-01-22 23:24:10 +0100

    insert new codes on js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+window.onload = function () {
     const text = document.getElementById("typing").innerText;
     document.getElementById("typing").innerText = "";
-  
+
     let i = 0;
     function type() {
         if (i < text.length) {
@@ -11,3 +11,21 @@ window.onload = function() {
         }
     }
     setTimeout(type, 2000);
+}
+
+window.onload = function () {
+    var galleryImages = document.querySelectorAll(".gallery-image");
+    var currentImageIndex = 0;
+
+    function nextImage() {
+        galleryImages[currentImageIndex].classList.remove("active");
+        currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
+        galleryImages[currentImageIndex].classList.add("active");
+    }
+
+
+    var nextButton = document.getElementById("next-button");
+    nextButton.addEventListener("click", nextImage);
+
+    setInterval(nextImage, 3000);
+}
