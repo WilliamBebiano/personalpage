447a9304eddcd0162f0a8485cb842fa4147c58df
William Bebiano <william.bebiano@hotmail.com>
2023-01-28 21:04:33 +0100

    update

--- script.js
+++ script.js
@@ -11,11 +11,9 @@ window.onload = function () {
         }
     }
     setTimeout(type, 2000);
-}
 
-window.onload = function () {
-    var galleryImages = document.querySelectorAll(".gallery-image");
-    var currentImageIndex = 0;
+    const galleryImages = document.querySelectorAll("#gallery-image");
+    let currentImageIndex = 0;
 
     function nextImage() {
         galleryImages[currentImageIndex].classList.remove("active");
@@ -24,8 +22,37 @@ window.onload = function () {
     }
 
 
-    var nextButton = document.getElementById("next-button");
+    const nextButton = document.getElementById("next-button");
     nextButton.addEventListener("click", nextImage);
 
     setInterval(nextImage, 3000);
+// back
+
+    function backImage() {
+        galleryImages[currentImageIndex].classList.remove("active");
+        currentImageIndex = (currentImageIndex - 1) % galleryImages.length;
+        galleryImages[currentImageIndex].classList.add("active");
+    }
+
+
+    const backButton = document.getElementById("back-button");
+    backButton.addEventListener("click", backImage);
+
+    //setInterval(nextImage, 3000);
+    
 }
+
+const parallaxContainer = document.querySelector('.parallax-container');
+const parallaxBackground = document.querySelector('.parallax-background');
+const parallaxForeground = document.querySelector('.parallax-foreground');
+
+
+// add event listener for scroll
+parallaxContainer.addEventListener('scroll', e => {
+  // calculate the scroll amount
+  const scrollPercentage = (parallaxContainer.scrollTop + parallaxContainer.clientHeight) / parallaxContainer.scrollHeight;
+
+  // update the background and foreground positions
+  parallaxBackground.style.transform = `translateY(${-scrollPercentage * 100}%)`;
+  parallaxForeground.style.transform = `translateY(${scrollPercentage * 50}%)`;
+});
