2138032aecc4eb079d6f04f8fd30041be37e2b00
William Bebiano <william.bebiano@hotmail.com>
2023-01-31 23:24:46 +0100

    game update

--- jogo.js
+++ jogo.js
@@ -14,6 +14,9 @@ astro1.src = './assets/atrotres-removebg.png'
 const meteoro = new Image()
 meteoro.src = './assets/meteoro1.png'
 
+const foguete = new Image()
+foguete.src = './assets/foguete2.png'
+
 const ready = new Image()
 ready.src = './assets/initial2.png'
 
@@ -23,7 +26,7 @@ const contexto = canvas.getContext('2d')
 /// Funcao de colisao astronauta e o chao 
 
 function fazColisao(astronaut, floor) {
-    const astronautY = astronaut.y + 180
+    const astronautY = astronaut.y + 100
     const floorY = floor.y
 
     if(astronautY >= floorY){
@@ -39,10 +42,10 @@ function createNewAstronaut() {
         spriteY: 229.9,
         width: 276,
         height: 619,
-        x: 0,
-        y:0,
-        newWidth: 80,
-        newHeight: 180,
+        x: 25,
+        y:10,
+        newWidth: 45,
+        newHeight: 85,
         
         pulo:4.6,
     
@@ -102,6 +105,7 @@ function createNewAstronaut() {
 
 
 //( definicao de background )
+function createBackground() {
 const background = {
     spriteX: 763,
     spriteY: 15,
@@ -109,12 +113,19 @@ const background = {
     height: 1026,
     x: 0,
     y:0,
-    newWidth: 1200,
-    newHeight: 602,
+    newWidth: 2800,
+    newHeight: 1200,
+    atualiza(){
+        const backgroundMove = 2
+        const repeatIn = background.width  / 1
+        const movement = background.x - backgroundMove
+
+        background.x = movement % repeatIn
+    },
     drawBackground() {
         /* se fosse necessrio completar o plano de fundo com alguma cor*/
-        contexto.fillStyle = '#00000'//cor ficticia
-        contexto.fillRect(0,0, canvas.width, canvas.height)
+        //contexto.fillStyle = '#00000'//cor ficticia
+        //contexto.fillRect(0,0, canvas.width, canvas.height)
 
         contexto.drawImage(
         sprites, // image é a imagem base do arquivo que trara a imagem a tela , no caso sprite
@@ -125,17 +136,19 @@ const background = {
 )
     }
 }
-
+return background
+}
 
 // (chao do jogo)
 function createFloor(){
+    
 const floor = {
     spriteX: 14,
     spriteY: 1264,
     width: 1476,
     height: 392,
     x: 0,
-    y:500,
+    y: canvas.height - 120,
     newWidth: 1800,
     newHeight: 200,
     atualiza(){
@@ -157,26 +170,196 @@ const floor = {
 }
 return floor
 }
-// ( ataques de cometa )
-const cometa = {
-    spriteX: 58,
-    spriteY: 17,
-    width: 510,
-    height: 363,
-    x: 900,
-    y: 10,
-    newWidth: 100,
-    newHeight: 50,
-    drawCometa() {
-        contexto.drawImage(
-        meteoro, // image é a imagem base do arquivo que trara a imagem a tela , no caso sprite
-        cometa.spriteX , cometa.spriteY, // sprite x e y
-        cometa.width,cometa.height, // tamanho do recorte na sprite
-        cometa.x,cometa.y, // ponto de inicio do personagem no canvas
-        cometa.newWidth, cometa.newHeight // tamanho da imagem que ira aparecer no canvas (redimensionada)
-)
+/*// ( ataques de cometa )
+function createCometa() {
+    const cometa = {
+        width: 510,
+        height: 363,
+        newWidth: 250,
+        newHeight: 200,
+
+
+        chao: {
+            spriteX: 58,
+            spriteY: 5,
+        },
+        ceu: {
+            spriteX: 52,
+            spriteY: 10,
+        },
+        espaco: 80,
+        drawCometa() {
+            
+        // const cometaCeuX = 600
+        // const cometaCeuY = 0
+            
+            cometa.pares.forEach(function(par){
+            const yRandom = par.y
+            const spaceBetweenCometa = 90
+            const cometaCeuX = par.x;
+            const cometaCeuY = yRandom;
+           
+        // (Cometa do Ceu)
+                contexto.drawImage(
+                meteoro, // image é a imagem base do arquivo que trara a imagem a tela , no caso sprite
+                cometa.ceu.spriteX, cometa.ceu.spriteY, // sprite x e y
+                cometa.width, cometa.height, // tamanho do recorte na sprite
+                cometaCeuX, cometaCeuY, // ponto de inicio do personagem no canvas
+                cometa.newWidth, cometa.newHeight // tamanho da imagem que ira aparecer no canvas (redimensionada)
+            )
+
+        /// (Cometa do chao)
+           const cometaChaoX = par.x
+            const cometaChaoY = cometa.newHeight + spaceBetweenCometa +yRandom
+            contexto.drawImage(
+                meteoro, // image é a imagem base do arquivo que trara a imagem a tela , no caso sprite
+                cometa.chao.spriteX, cometa.chao.spriteY, // sprite x e y
+                cometa.width, cometa.height, // tamanho do recorte na sprite
+                cometaChaoX, cometaChaoY, // ponto de inicio do personagem no canvas
+                cometa.newWidth, cometa.newHeight // tamanho da imagem que ira aparecer no canvas (redimensionada)
+            )
+
+            par.cometaCeu = {
+                x: cometaCeuX,
+                y: cometa.height + cometaCeuY
+            }
+
+            par.cometaChao = {
+                x: cometaChaoX,
+                y: cometaChaoY
+            }
+
+             })
+        },
+        pares: [],
+        atualiza() {
+          const passou100frames =  frames & 100 ===0
+            if(passou100frames){
+                cometa.pares.push({
+                x: 600,
+                y: -150 * (Math.random() + 1),
+                })
+            }
+
+            cometa.pares.forEach(function(par){
+                par.x = par.x - 2
+            })
+        }
+    
     }
+    return cometa
+}*/
+// Nave espacial 
+function createNave() {
+    const nave = {
+        width: 68.5,
+        height: 281,
+        chao: {
+            spriteX: 3,
+            spriteY: 1,
+        },
+        ceu: {
+            spriteX: 69,
+            spriteY: 1,
+        },
+        espaco: 80,
+        drawNave() {
+
+
+            nave.pares.forEach(function(par) {
+
+                const yRandom = par.y
+                const spaceBetweenNave = 160
+
+                const naveCeuX = par.x
+                const naveCeuY = yRandom
+
+
+                // (Cometa do Ceu)
+                contexto.drawImage(
+                    foguete, // image é a imagem base do arquivo que trara a imagem a tela , no caso sprite
+                    nave.ceu.spriteX, nave.ceu.spriteY, // sprite x e y
+                    nave.width, nave.height, // tamanho do recorte na sprite
+                    naveCeuX, naveCeuY, // ponto de inicio do personagem no canvas
+                    nave.width, nave.height // tamanho da imagem que ira aparecer no canvas (redimensionada)
+                )
+
+                /// (Cometa do chao)
+                const naveChaoX = par.x
+                const naveChaoY = nave.height + spaceBetweenNave + yRandom
+                contexto.drawImage(
+                    foguete, // image é a imagem base do arquivo que trara a imagem a tela , no caso sprite
+                    nave.chao.spriteX, nave.chao.spriteY, // sprite x e y
+                    nave.width, nave.height, // tamanho do recorte na sprite
+                    naveChaoX, naveChaoY, // ponto de inicio do personagem no canvas
+                    nave.width, nave.height // tamanho da imagem que ira aparecer no canvas (redimensionada)
+                )
+
+                par.naveCeu = {
+                      x: naveCeuX,
+                      y: nave.height + naveCeuY
+                  },
+      
+                  par.navechao = {
+                      x: naveChaoX,
+                      y: naveChaoY
+                  }
+
+            })
+        },
+
+        temColisaoComOAstronauta(par){
+
+            const cabecaDoAstronaut = globais.astronaut.y
+            const peDoAstronaut = globais.astronaut.y + globais.astronaut.newHeight
+
+            if((globais.astronaut.x + globais.astronaut.newWidth >= par.x) ){
+                if( cabecaDoAstronaut <= par.naveCeu.y){
+                    return true
+                }
+                
+                if(peDoAstronaut >= par.navechao.y){
+                    return true
+                }
+            }
+            return false
+
+        },
+        pares: [],
+
+        
+
+        atualiza() {
+            const passou100frames = frames % 100 === 0
+            if (passou100frames) {
+                console.log('passou 100 frames')
+                nave.pares.push({
+                    x: canvas.width - 100,
+                    y: -70 * (Math.random() + 1),
+                })
+            }
+
+            nave.pares.forEach(function(par) {
+                par.x = par.x - 2
+
+                if (nave.temColisaoComOAstronauta(par)) {
+                    console.log('voce perdeu')
+                    mudaParaTela(telas.INICIO)
+
+                }
+
+                if (par.x + nave.width <= 0) {
+                    nave.pares.shift()
+                }
+            }
+            )
+        }
+
+    }
+    return nave
 }
+
+
 // Tela de inicio 
 const inicio = {
     spriteX: 29.1,
@@ -215,15 +398,19 @@ function mudaParaTela(novaTela) {
 const telas = {
     INICIO: {
         inicializa() {
+           globais.background = createBackground()
            globais.astronaut = createNewAstronaut() 
            globais.floor = createFloor()
+           //globais.cometa = createCometa()
+           globais.nave = createNave()
+
         },
         draw() {
-            background.drawBackground()
-            //floor.drawFloor()
-            //astronaut.drawAstronaut()
+            globais.background.drawBackground()
             globais.floor.drawFloor()
-           // globais.astronaut.drawAstronaut()
+            //globais.nave.drawNave()
+            globais.astronaut.drawAstronaut()
+            
             inicio.drawInicio()
             
 
@@ -233,6 +420,10 @@ const telas = {
         },
         atualiza() {
             globais.floor.atualiza()
+            //globais.nave.atualiza()
+            
+            
+            
 
         }
     }
@@ -240,18 +431,23 @@ const telas = {
 
 telas.JOGO = {
     draw() {
-        background.drawBackground()
+        globais.background.drawBackground()
         globais.floor.drawFloor()
         globais.astronaut.drawAstronaut()
-        cometa.drawCometa()
+        //globais.cometa.drawCometa()
+        globais.nave.drawNave()
     },
     click() {
         globais.astronaut.jump()
 
     },
     atualiza() {
+        //globais.cometa.atualiza()
         globais.astronaut.atualiza()
         globais.floor.atualiza()
+        globais.background.atualiza()
+        globais.nave.atualiza()
+        
     }
 }
 
